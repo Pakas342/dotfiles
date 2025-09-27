59d56993399a57217400947a3920752af2ca5a18
Pakas342 <jcbp1999@gmail.com>
2025-09-27 23:50:40 +1000

    initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Key bindings fot autocompletion
+bind \cy accept-autosuggestion
+bind \cf forward-word
+
+# Key bindingd for history nav
+bind \cp up-or-search
+bind \cn down-or-search
+
+set fish_greeting
